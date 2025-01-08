de9fee00ad5951c06fe931ab5e47216818495e65
Drei Abmab <dreiabmab@gmail.com>
2025-01-08 23:53:34 +0800

    add patient form

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Apex Charts - Line Chart and Bar Chart
 React-Apex Charts - Access to ApexChart Components 
 gh-pages - for testing live demo pages in github
 React-router-dom - For site routing 
+mui-tel-input - phone number input
--- src/pages/Patients/Patients.jsx
+++ src/pages/Patients/Patients.jsx
@@ -9,7 +9,11 @@ import useDebounce from "../../hooks/useDebounce";
 import RowsPerPage from "../../components/RowsPerPage";
 import Popup from "../../components/PopUps/Popup";
 import usePopup from "../../hooks/usePopUp";
-import { Alert } from "bootstrap";
+import ReactDOM from "react-dom";
+import { Box, Chip, FormControl, FormHelperText, Input, InputLabel, MenuItem, OutlinedInput, Select, TextField, useTheme } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MuiTelInput } from "mui-tel-input";
 
 const Patients = () => {
   const [filters, setFilters] = useState({
@@ -100,22 +104,21 @@ const Patients = () => {
     <>
       {/* Import File Popup */}
       {showImportPopup && (
-          <Popup
-            type="Import"
-            title="Import Patient List"
-            message={`This action can be only done once`}
-            icon={ImportIcon}
-            onConfirm={closeImportPopUp}
-            onCancel={closeImportPopUp}
-            confirmLabel="Confirm"
-            cancelLabel="Cancel"
-            isExiting={isExiting}
-            customClass="import-popup"
+        <Popup
+          type="Import"
+          title="Import Patient List"
+          message={`This action can be only done once`}
+          icon={ImportIcon}
+          onConfirm={closeImportPopUp}
+          onCancel={closeImportPopUp}
+          confirmLabel="Confirm"
+          cancelLabel="Cancel"
+          isExiting={isExiting}
+          customClass="import-popup"
         />
       )}
+      <AddPatients />
 
-
-        
       <div className="patients auto-sizing">
         <FilterBlock filters={filters} onFilterChange={handleFilterChange} patientInfo={updatedPatientInfo} />
 
@@ -143,6 +146,279 @@ const Patients = () => {
     </>
   );
 };
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const allergiedList = [
+  'Colophonium',
+  'Latex',
+  'Tree Nuts',
+  'Resins',
+  'Food',
+  'Artificial Flavoring',
+  'Red Dye',
+  'None',
+];
+
+const illnessList = [
+  'ADD/ADHD',
+  'Diabetes',
+  'Epilepsy/Seizure',
+  'Asthma',
+  'Heart Murmur',
+  'Plavix (chopidogerel)',
+  'Blood Disorder / Hemophilia',
+  'Congenital Heart Disease/Defect',
+  'Heart Atteck/Heart Failure',
+  'Artificial Heart Valve',
+  'Tuberculosis',
+  'Hepatitis/Liver Disease',
+  'Biophosphonate Thearapy (Oral or IV)',
+  'Costisone - Corticosteroid Thearapy',
+  'Systemic Lupus Erythematosus',
+  'None',
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight: personName.includes(name)
+      ? theme.typography.fontWeightMedium
+      : theme.typography.fontWeightRegular,
+  };
+}
+
+
+const AddPatients = ({ }) => {
+  const [gender, setGender] = useState(""); // State to track the selected gender
+  const [race, setRace] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [insurance, setInsurance] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const theme = useTheme();
+  const [alergieName, setAlergieName] = React.useState([]);
+
+  const handlePhoneChange = (value) => {
+    setPhoneNumber(value)
+  }
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+
+  }
+
+  const handleRaceChange = (event) => {
+    setRace(event.target.value)
+    
+  }
+
+  const handleInsuaranceChange = (event) => {
+    setInsurance(event.target.value)
+    
+  }
+
+  const handleAlergieNameChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setAlergieName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+
+  }
+  return ReactDOM.createPortal(
+    <div className="new_patient_containter f-center">
+      <div className="new_patients glassmorphism shadow">
+        <form action="">
+          <h1>New Patient</h1>
+
+          <h3>Patient Information</h3>
+          <div className="patient_info container">
+            <div className="fname">
+              <label htmlFor="fname">First name</label>
+              <TextField fullWidth id="outlined-basic" variant="outlined" />
+            </div>
+
+            <div className="lname">
+              <label htmlFor="lname">Last name</label>
+              <TextField fullWidth id="outlined-basic" variant="outlined" />
+            </div>
+
+            <div className="gender">
+              <label htmlFor="lname">Gender</label>
+              <FormControl fullWidth>
+                <Select
+                  labelId="gender-label"
+                  value={gender}
+                  onChange={handleGenderChange}
+                  displayEmpty
+                >
+                  <MenuItem value="" disabled>
+                    Select Gender
+                  </MenuItem>
+                  <MenuItem value="Male">Male</MenuItem>
+                  <MenuItem value="Female">Female</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+
+            <div className="birthdate">
+              <label htmlFor="birthdate">Birth Date</label>
+              <LocalizationProvider dateAdapter={AdapterDayjs} fullWidth>
+                <DatePicker
+                  value={selectedDate}
+                  onChange={(newValue) => setSelectedDate(newValue)}
+                  renderInput={(params) => <TextField {...params} />}
+                  fullWidth
+                />
+              </LocalizationProvider>
+            </div>
+
+            <div className="race">
+              <label htmlFor="race">Ethnicity</label>
+              <FormControl fullWidth>
+                <Select
+                  labelId="race-label"
+                  value={race}
+                  onChange={handleRaceChange}
+                  displayEmpty
+                >
+                  <MenuItem value="" disabled>
+                    Select Ethnicity
+                  </MenuItem>
+                  <MenuItem value="White">White</MenuItem>
+                  <MenuItem value="African_American">African American</MenuItem>
+                  <MenuItem value="Hispanic">Hispanic</MenuItem>
+                  <MenuItem value="Asian">Asian</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+
+            <div className="insurance">
+              <label htmlFor="insurance">Insurance</label>
+              <FormControl fullWidth>
+                <Select
+                  labelId="insurance-label"
+                  value={insurance}
+                  onChange={handleInsuaranceChange}
+                  displayEmpty
+                >
+                  <MenuItem value="" disabled>
+                    Select Insurance
+                  </MenuItem>
+                  <MenuItem value="insurance_1">insurance_1</MenuItem>
+                  <MenuItem value="insurance_2">insurance_2</MenuItem>
+                  <MenuItem value="insurance_3">insurance_3</MenuItem>
+                  <MenuItem value="insurance_4">insurance_4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+
+            <div className="language_spoken">
+              <label htmlFor="language_spoken">Language Spoken</label>
+              <TextField fullWidth id="outlined-basic" variant="outlined" />
+            </div>
+
+            <div className="language_spoken_at_hone">
+              <label htmlFor="language_spoken_at_hone">Language Spoken At Home</label>
+              <TextField fullWidth id="outlined-basic" variant="outlined" />
+            </div>
+          </div>
+
+          <h3>Gaurdian Information</h3>
+          <div className="gaurdian_info container">
+            <div className="gname">
+              <label htmlFor="gname">Gaurdian Name</label>
+              <TextField id="outlined-basic" label="Gaurdian Name" variant="outlined" />
+            </div>
+
+            <div className="grelationship">
+              <label htmlFor="grelationship">Relationship</label>
+              <TextField id="outlined-basic" label="e.g Father/Mother/Auntie" variant="outlined" />
+            </div>
+
+            <div className="gnumber">
+              <label htmlFor="gnumber">Phone Number</label>
+              <MuiTelInput
+                value={phoneNumber}
+                onChange={handlePhoneChange}
+              />
+            </div>
+
+            <div className="gemail">
+              <label htmlFor="gemail">Gaurdian Email</label>
+              <TextField id="outlined-basic" label="Gaurdian Email" variant="outlined" type="email" />
+            </div>
+          </div>
+
+          <h3>Medical History</h3>
+          <div className="medical_history container">
+            <div className="allergies gls">
+              <label htmlFor="allergies">Allergies</label>
+              <FormControl fullwidth>
+                <Select
+                  labelId="demo-multiple-chip-label"
+                  id="demo-multiple-chip"
+                  multiple
+                  value={alergieName}
+                  onChange={handleAlergieNameChange}
+                  input={<OutlinedInput id="select-multiple-chip" fullwidth/>}
+                  renderValue={(selected) => (
+                    <Box fullwidth sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} fullwidth/>
+                      ))}
+                    </Box>
+                  )}
+                  MenuProps={MenuProps}
+                  fullwidth
+                >
+                  {allergiedList.map((allergie) => (
+                    <MenuItem
+                      key={allergie}
+                      value={allergie}
+                      style={getStyles(allergie, alergieName, theme)}
+                    >
+                      {allergie}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+
+            <div className="doctor">
+
+            </div>
+
+            <div className="dentist">
+
+            </div>
+
+            <div className="medication">
+
+            </div>
+
+            <div className="illness">
+
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>,
+    document.body
+  );
+}
 
 const UserButtons = ({ openImportPopUp }) => (
   <div className="user_buttons">
